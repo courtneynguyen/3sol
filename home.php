f4cdb1a32d2772f0ddb626a51ed87565b58362c7
root <root@localhost.localdomain>
2014-11-02 13:42:25 -0700

    new content, and styling

--- home.php
+++ home.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
-	<div id="content" class="container">
-	<h1 class="center-txt">Web Developer</h1>
+	<div id="content" class="container sm-container content">
+	<h3 class="no-btm no-tp">I am a lion tamer (with shoelace), but </h3>
+<h1 class="no-tp">my passion is to code, quest, and cat.</h1>
 	<div class="main-content">
-	
-		<div id="Parallax">
+<h4 class="">What do I do for a living?</h4> 
+<p class="">In a nutshell, I play in a digital sandbox every day. I invent interfaces the imagination only dreams of. I am a web engineer. What is my job? I tinker, create, design, refine, repeat. I wish to create beautiful user experiences that are transparent, friendly, and usable for the world.</p>
+		<!--<div id="Parallax">
 		
 			<img src="/wp-content/themes/3sol/images/Bottom.png"/>
 			<img src="/wp-content/themes/3sol/images/Top.png"/>
-		</div>
+		</div> -->
 	</div>
 	</div>
 
